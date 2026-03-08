f2f4a0ba3a2cd45346e7a3023ab303f4d0fed94c
MASA\masam <masamasayy518@gmail.com>
2026-03-08 22:21:47 +0900

    in

--- window/window_usjp_rate.py
+++ window/window_usjp_rate.py
@@ -9,7 +9,10 @@ class UsdFxWindow:
         
         self.__log = Logger("UsdFxWindow")
         self.__tk = TkLib("UsdJpy画面", "700x800")
+        self.__run_window()
         
     def __run_window(self):
-        
+
+        self.__log.print_info("usd-jpy windw start")
+        self.__tk.tkinter_label("Fx UsdJpy", 0.1, 0.05, "center")
         
